f1df04cf1e1652fe64ffbd605b003623f817e0dc
Joko <jqnestevan@gmail.com>
2025-03-03 22:05:13 +0100

    Hecho las 2 primeras funciones de Tablero

--- hada-p2/Tablero.cs
+++ hada-p2/Tablero.cs
@@ -4,9 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hada_p2
+namespace Hada
 {
     internal class Tablero
     {
+        public int TamTablero { get; }
+
+        private List<Coordenada> coordenadasDisparadas;
+        private List<Coordenada> coordenadasTocadas;
+        private List<Barco> barcos;
+        private List<Barco> barcosEliminados;
+        private Dictionary<Coordenada, string> casillasTablero;
+
+        public Tablero(int tamTablero, List<Barco> barcos)
+        {
+            if (tamTablero >= 4 && tamTablero <= 9)
+            {
+                TamTablero = tamTablero;
+            }
+            else
+            {
+                throw new Exception();
+            }
+            this.barcos = new List<Barco>(barcos);
+            coordenadasDisparadas = new List<Coordenada>();
+            coordenadasTocadas = new List<Coordenada>();
+            barcosEliminados = new List<Barco>();
+            casillasTablero = new Dictionary<Coordenada, string>();
+        }
+
+        private void iniciaCasillasTablero()
+        {
+            for (int i = 0; i < TamTablero; i++)
+            {
+                for (int j = 0; j < TamTablero; j++)
+                {
+                    foreach (var barco in barcos)
+                    {
+                        Coordenada casilla = new Coordenada(i, j);
+                        if (barco.CoordenadasBarco.ContainsKey(casilla))
+                        {
+                            casillasTablero.Add(casilla, barco.Nombre);
+                        }
+                        else
+                        {
+                            casillasTablero.Add(casilla, "AGUA");
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Disparar(Coordenada c)
+        {
+            if (c.Columna > TamTablero && c.Fila > TamTablero)
+            {
+                foreach (var barco in barcos)
+                {
+                    if (barco.CoordenadasBarco.ContainsKey(c))
+                    {
+                        coordenadasDisparadas.Add(c);
+                        //barco.disparar(c);
+                    }
+
+                }
+            }
+            else
+            {
+                Console.Write("La coordenada (" + c.Fila + "," + c.Columna + ") está fuera de\r\nlas dimensiones del tablero.");
+            }
+        }
+
+
     }
-}
