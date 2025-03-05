4aa8f2cacaa66f3890bac0a6d7bf429b831c7ef4
asm312-ua <asm312@alu.ua.es>
2025-03-05 07:15:53 +0100

    24437144N Modified Eventos, cambiada la implementacion de eventos anterior a una "mas correcta" falta probarla.

--- hada-p2/Eventos.cs
+++ hada-p2/Eventos.cs
@@ -6,19 +6,44 @@ using System.Threading.Tasks;
 
 namespace Hada
 {
-    public class TocadoArgs : EventArgs
+    public class TocadoEventArgs : EventArgs
     {
-        public TocadoArgs(string nombre, string coordenadas)
-        {
-            Console.Write($"TABLERO: Barco {nombre} tocado en Coordenada: [{coordenadas}]");
-        }
-        
+        public string Nombre { get;  set; }
+        public string Coordenadas { get;  set; }
     }
-    public class HundidoArgs : EventArgs
+
+    public class TocadoArgs
     {
-        public HundidoArgs(string nombre)
+        public delegate void TocadoEventHandler(object sender, TocadoEventArgs args);
+        public event TocadoEventHandler Tocado;
+
+        public void TocarBarco(string name, string coordenadas)
         {
-            Console.Write(", y ha sido hundido");
+            OnTocado(name, coordenadas);
+        }
+
+        protected virtual void OnTocado(string nombre, string coordenadas)
+        {
+            Tocado?.Invoke(this, new TocadoEventArgs() { Nombre = nombre, Coordenadas = coordenadas });
         }
     }
+    public class HundidoEventArgs : EventArgs
+    {
+        public string Nombre { get; set; }
+    }
+
+    public class HundidoArgs
+    {
+        public delegate void HundidoEventHandler(object sender, HundidoEventArgs args);
+        public event HundidoEventHandler Hundido;
+        public void HundirBarco(string name)
+        {
+            OnHundido(name);
+        }
+        protected virtual void OnHundido(string nombre)
+        {
+            Hundido?.Invoke(this, new HundidoEventArgs() { Nombre = nombre });
+        }
+    }
+
 }
